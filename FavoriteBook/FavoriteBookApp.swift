2874cd510977aa6cfaf24915f306638c474a0c3c
Furkan Cingöz <efcingoz@outlook.com>
2023-10-30 19:53:29 +0300

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by Furkan Cingöz on 30.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
